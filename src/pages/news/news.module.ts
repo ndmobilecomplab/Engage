b2330de2db8d0a2b35c6283cde0bc45e50914ad4
Molly Zachlin <mzachlin@gmail.com>
2019-03-28 17:29:08 -0400

    Worked on pulling data from Firebase... experiencing issues

--- src/pages/news/news.module.ts
+++ src/pages/news/news.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { NewsPage } from './news';
 
+
 @NgModule({
   declarations: [
     NewsPage,
--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FirebaseAuthProvider } from '../../providers/firebase-auth/firebase-auth';
+import { FirebaseDatabaseProvider } from '../../providers/firebase-database/firebase-database';
+import { Firebase }
+
 
 /**
  * Generated class for the NewsPage page.
@@ -13,6 +17,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-news',
   templateUrl: 'news.html',
 })
+
 export class NewsPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
@@ -20,6 +25,13 @@ export class NewsPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad NewsPage');
+    var posts = {};
+    //Creates a listener at the news part of the database
+    const postRef: firebase.database.Reference = firebase.database().ref('/news/');
+    postRef.on('value', postSnapshot => {
+      posts = postSnapshot.val();
+    });
+
   }
 
 }
