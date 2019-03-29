dc0c0d0f652696e80192b335b825a3005e35fbed
Molly Zachlin <mzachlin@gmail.com>
2019-03-29 17:08:07 -0400

    Created a Post model, worked on pulling data from Firebase for the news page

--- src/pages/news/news.ts
+++ src/pages/news/news.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { FirebaseAuthProvider } from '../../providers/firebase-auth/firebase-auth';
 import { FirebaseDatabaseProvider } from '../../providers/firebase-database/firebase-database';
-import { Firebase }
+import * as firebase from 'Firebase'
+import Post from '../../models/post';
+import { Observable } from 'rxjs/Observable';
 
 
 /**
@@ -20,17 +22,23 @@ import { Firebase }
 
 export class NewsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  key: string;
+
+  posts$: Observable<Post>;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private firebase: FirebaseDatabaseProvider) {
+    this.key = navParams.get('posts');
+    this.posts$ = firebase.getEvent(this.key);
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad NewsPage');
-    var posts = {};
+    /*console.log('ionViewDidLoad NewsPage');
+    //var posts = {};
     //Creates a listener at the news part of the database
     const postRef: firebase.database.Reference = firebase.database().ref('/news/');
     postRef.on('value', postSnapshot => {
-      posts = postSnapshot.val();
-    });
+      this.posts$ = postSnapshot.val();
+    }); */
 
   }
 
